4a09b73ae2d4e8d43ecfd34c763b95d67928b0c3
DarkEyesTheBaker <julesdietsch@fioptics.com>
2021-03-24 15:07:37 -0400

    Complex Conditionals

--- ComplexConditional2/src/ComplexCondtional2Class.java
+++ ComplexConditional2/src/ComplexCondtional2Class.java
@@ -3,11 +3,16 @@ import java.util.Scanner;
 public class ComplexCondtional2Class {
 
 	public static void main(String[] args) {
+		System.out.print("Enter data: ");
 		Scanner reader = new Scanner(System.in);
 		String input = reader.next();
 		String sOne = "abc";
 		String sTwo = "z";
-		if (input.contains(sOne)  &&  input.contains(sTwo)) {
+	
+		boolean bool1 = input.contains(sOne);
+		boolean bool2 = input.contains(sTwo);
+		
+		if ((input.contains(sOne)  &&  input.contains(sTwo)) && false) {
 			System.out.println("TRUE!");
 		} else {
 			System.out.println("FALSE!");
