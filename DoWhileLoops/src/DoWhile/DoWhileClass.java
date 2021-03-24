8edb9fb0f4f6cb4d20742cb0f5e89f52d57aa310
DarkEyesTheBaker <julesdietsch@fioptics.com>
2021-03-24 15:38:13 -0400

    While and Do-While Loops

---
+++ DoWhileLoops/src/DoWhile/DoWhileClass.java
@@ -0,0 +1,13 @@
+package DoWhile;
+
+public class DoWhileClass {
+
+	public static void main(String[] args) {
+		int i = 5;
+		while(i>0)
+		{
+			System.out.println("Hello, World!");
+			i--;
+		}
+	}
+}
