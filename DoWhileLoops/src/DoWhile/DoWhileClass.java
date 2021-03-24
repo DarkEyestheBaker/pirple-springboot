97afd9005cf279c917661500a93562fde6b1b57f
DarkEyesTheBaker <julesdietsch@fioptics.com>
2021-03-24 15:49:27 -0400

    Do and Do While Loops

--- DoWhileLoops/src/DoWhile/DoWhileClass.java
+++ DoWhileLoops/src/DoWhile/DoWhileClass.java
@@ -1,13 +1,20 @@
 package DoWhile;
 
+import java.util.*;
+
 public class DoWhileClass {
 
 	public static void main(String[] args) {
-		int i = 5;
-		while(i>0)
-		{
-			System.out.println("Hello, World!");
-			i--;
-		}
+		System.out.print("Enter data: ");
+		Scanner reader = new Scanner(System.in);
+		String input;
+		String all = "";
+		
+		do {
+			input =reader.nextLine();
+			all += input;
+		} 
+		while(!input.equals("STOP"));
+		System.out.println(all);
 	}
 }
