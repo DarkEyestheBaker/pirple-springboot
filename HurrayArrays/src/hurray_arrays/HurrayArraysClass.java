5fff2df86926c389ec689b9571d1e53eac4b36b7
DarkEyesTheBaker <julesdietsch@fioptics.com>
2021-03-30 21:02:01 -0400

    Arrays

---
+++ HurrayArrays/src/hurray_arrays/HurrayArraysClass.java
@@ -0,0 +1,19 @@
+package hurray_arrays;
+
+import java.util.Arrays;
+
+public class HurrayArraysClass {
+
+	public static void main(String[] args) {
+		//97
+		char[] alpha = new char[26];
+		
+		for(int i = 0; i < 26; i++) 
+		{
+			alpha[i] = (char) (97 + i); 
+		}
+		System.out.println(Arrays.toString(alpha));
+
+	}
+
+}
