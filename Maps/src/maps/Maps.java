13b2039fb3c8bb753b5e585439426b4fe9fa4cbf
DarkEyesTheBaker <julesdietsch@fioptics.com>
2021-05-25 13:05:39 -0400

    Maps

---
+++ Maps/src/maps/Maps.java
@@ -0,0 +1,30 @@
+package maps;
+
+import java.util.*;
+
+public class Maps {
+
+	public static void main(String[] args) {
+		String[] allNames = 
+				//<editor-fold desc="raw names data">
+			{"Jane", "Addams",
+				"Muhammad", "Ali",
+				"Stephen", "Ambrose",
+				"Louis", "Armstrong",
+				"Joan", "Baez", 
+				"Josephine", "Baker",
+				"Clyde", "Barrow",
+				"Elizabeth", "Blackwell",
+				"Molly", "Brown",
+				"Rachel","Carson",
+				"Fidel", "Castro",
+				"Coco", "Chanel", 
+				"Charlie", "Chaplin",
+				
+				
+				
+			}
+
+	}
+
+}
